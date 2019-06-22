e144aa25bf9c739fe4ddf533b94d3074bfba7c55
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-06-21 23:02:06 -0500

    changed where images go, created an array for games, made it so a for loop puts a button on for each game.  Working on the AJAX pull

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,46 @@
 var apikey = "6Iw6Cta0oCIkkg1qd9wk9LjmDN5tfZM4"
 
-// q = grabbed from text input box.
-// limit = number of images to grab
+// List of Video Games to go up top
+
+var games = ["Assassin's Creed", "God of War", "Destiny 2", "Apex Legends", "Super Mario Bros.", "Final Fantasy", "World of Warcraft", "HALO", "Forza Motorsports", "Sonic the Hedgehog"]
+// Creating buttons to put up top
+for (var i = 0; i < games.length; i++) {
+    var btn = $("<button>")
+    btn.text(games[i]);
+    btn.attr("data-game", games[i]);
+    $("#buttons").append(btn);
+}
+
+
+// on click
+
+$("button").on("click", function () {
+    // q = grabbed from text input box.
+    // limit = number of images to grab
+
+
+    var videoGame = $(this).attr("data-game");
+    console.log(videoGame);
+    // query URL
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + videoGame + "&api_key=" + apikey + "&limit=5";
+
+
+    // AJAX query
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        
+        console.log(queryURL);
+        console.log(response);
+
+        var results = response.data;
+
+        for (var i =0; i<results.length; i++){
+            
+        }
+    })
+
+})
